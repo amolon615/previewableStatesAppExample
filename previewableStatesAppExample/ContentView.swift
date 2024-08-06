4609ff14ff288c07edbb0fa9fa155b66a3f026ab
Artem <114776102+amolon615@users.noreply.github.com>
2024-08-06 10:05:26 +0100

    Initial Commit

---
+++ previewableStatesAppExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  previewableStatesAppExample
+//
+//  Created by amolonus on 06/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ previewableStatesAppExample/previewableStatesAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  previewableStatesAppExampleApp.swift
+//  previewableStatesAppExample
+//
+//  Created by amolonus on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct previewableStatesAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
